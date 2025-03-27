057b156ff710ec1333037e867da866167c088be2
Ali Ayman <aliaymank2005@gmail .com>
2025-03-27 14:47:46 +0200

    added structs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include<string>
 using namespace std;
-int main()
-{
-  cout << "this ssis awlad el 7ag ragab";
-  system("pause");
-}
+const int Max = 100;
+struct customer {
+    int Id;
+    string password;
+    string name;
+    string phone;
+    string location;
+};
+customer customers[Max];
+int customer_count = 0;
+struct date {
+    int day, month, year;
+};
+struct product {
+    int code, quantity;
+    string name;
+    string Category;
+    date productionDate;
+    date expirationDate;
+    double price;
+};
+product products[Max];
+int product_count = 0;
+struct order {
+    int customerId;
+    string productName[Max];
+    double totalPrice;
+    int numProducts;
+};
