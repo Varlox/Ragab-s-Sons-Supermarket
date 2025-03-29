e496ef37b3c01beaa2d46443fcd85e006526b69a
Varlox <anashatem77@gmail.com>
2025-03-29 18:36:31 +0200

    feat: implement signup and login functionality with file storage

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 const int Max = 100;
 struct customer
@@ -35,24 +36,104 @@ struct order
   int numProducts;
 };
 
-void login(customer &c)
+void signup(customer &c)
 {
+  cout << "Enter your ID: ";
+  cin >> c.Id;
+  cout << "Enter your password: ";
+  cin >> c.password;
+  cout << "Enter your name: ";
+  cin >> c.name;
+  cout << "Enter your phone number: ";
+  cin >> c.phone;
+  cout << "Enter your location: ";
+  cin >> c.location;
+  customers[customer_count] = c;
+  customer_count++;
+  cout << "Signup successful!" << endl;
 
-  do
+  ofstream file("customers.txt", ios::app); // Open file in append mode
+  if (file.is_open())
   {
-    cout << "Enter your ID: ";
-    cin >> c.Id;
-    cout << "Enter your password: ";
-    cin >> c.password;
-    if (c == customers[0].Id && c.password == customers[0].password)
+    file << c.Id << " " << c.password << " " << c.name << " " << c.phone << " " << c.location << endl;
+    file.close();
+    cout << "Signup successful! Data saved to file." << endl;
+  }
+  else
+  {
+    cout << "Error: Unable to open file to save data." << endl;
+  }
+}
+
+void login()
+{
+  int inputId;
+  string inputPassword;
+  bool loginSuccessful = false;
+
+  cout << "Enter your ID: ";
+  cin >> inputId;
+  cout << "Enter your password: ";
+  cin >> inputPassword;
+
+  ifstream file("customers.txt"); // Open the file for reading
+  if (file.is_open())
+  {
+    int id;
+    string password, name, phone, location;
+
+    // Read each line from the file and check credentials
+    while (file >> id >> password >> name >> phone >> location)
     {
-      cout << "Login successful!" << endl;
-      cout << "Welcome, " << customers[0].name << "!" << endl;
-      continue;
+      if (id == inputId && password == inputPassword)
+      {
+        cout << "Login successful!" << endl;
+        cout << "Welcome, " << name << "!" << endl;
+        loginSuccessful = true;
+        break;
+      }
     }
-    else
+    file.close();
+
+    if (!loginSuccessful)
     {
       cout << "Invalid ID or password." << endl;
     }
-  } while (c != customers[0].Id && c.password != customers[0].password);
+  }
+  else
+  {
+    cout << "Error: Unable to open file to verify login." << endl;
+  }
+}
+
+int main()
+{
+  int choice;
+  customer c;
+  do
+  {
+    cout << "1. Signup" << endl;
+    cout << "2. Login" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+      signup(c);
+      break;
+    case 2:
+      login();
+      break;
+    case 3:
+      cout << "Exiting..." << endl;
+      break;
+    default:
+      cout << "Invalid choice. Please try again." << endl;
+      break;
+    }
+  } while (choice != 3);
+
+  return 0;
 }
